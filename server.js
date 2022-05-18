b2fd550a5aa441118e01d335c4f1e63629af3857
StevenM <stevenbmendoza@gmail.com>
2022-05-17 22:43:43 -0700

    connect to mongoose db or heroku mongo db

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require('express');
+const express = require("express");
+// require backend database language functions mongoDB
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
+
+// select which Mongoose db to connect to, use _URI to deploy to heroku
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// use mongo queries logs
+mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
