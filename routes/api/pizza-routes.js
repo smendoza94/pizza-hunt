2e47beb15d752b77a7f533c28770c452eadbb458
StevenM <stevenbmendoza@gmail.com>
2022-05-19 15:56:43 -0700

    spelling error in routes

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -9,9 +9,9 @@ const {
 } = require("../../controllers/pizza-controller");
 
 // set up all GET and POST at /api/pizzas
-router.router("/").get(getAllPizza).post(createPizza);
+router.route("/").get(getAllPizza).post(createPizza);
 
 // set up GET one, PUT, and DELETE at /api/pizzas/:id
-router.router("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
+router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
 
 module.exports = router;
