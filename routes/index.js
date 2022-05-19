85e8b9f4b00d1572c587be0a504efa6bfdf6a446
StevenM <stevenbmendoza@gmail.com>
2022-05-19 15:43:55 -0700

    connect api routes to express

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-const router = require('express').Router();
-const htmlRoutes = require('./html/html-routes');
+const router = require("express").Router();
+const htmlRoutes = require("./html/html-routes");
+// import all api routes from api/index, auto finds index.js
+const apiRoutes = require("./api");
 
-router.use('/', htmlRoutes);
+// add prefix of '/api' to all routes in 'api'
+router.use("/api", apiRoutes);
+router.use("/", htmlRoutes);
 
 router.use((req, res) => {
-  res.status(404).send('<h1>😝 404 Error!</h1>');
+  res.status(404).send("<h1>😝 404 Error!</h1>");
 });
 
 module.exports = router;
