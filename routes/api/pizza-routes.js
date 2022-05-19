b708a1d2962179b1bb7e584bf88534d5b0a4891d
StevenM <stevenbmendoza@gmail.com>
2022-05-19 15:28:16 -0700

    function routes for pizza

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -9,9 +9,9 @@ const {
 } = require("../../controllers/pizza-controller");
 
 // set up all GET and POST at /api/pizzas
-router.router("/").get().post();
+router.router("/").get(getAllPizza).post(createPizza);
 
 // set up GET one, PUT, and DELETE at /api/pizzas/:id
-router.router("/:id").get().put().delete();
+router.router("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
 
 module.exports = router;
