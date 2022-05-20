fb5c7307bb0ba7561e3cbf8d2fcc1e27b1b51dc9
StevenM <stevenbmendoza@gmail.com>
2022-05-19 21:24:01 -0700

    mongo connection to ip not localhost

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(require("./routes"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/pizza-hunt", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
