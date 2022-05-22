9fc9db172c31f1af9539a75dec3d80893d86ae3f
StevenM <stevenbmendoza@gmail.com>
2022-05-21 17:13:09 -0700

    Comment model and pizza relation, comment create and remove controller

---
+++ controllers/comment-controller.js
@@ -0,0 +1,53 @@
+const { Comment, Pizza } = require("../models");
+
+const commentController = {
+  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+  // remove comment from pizza
+  // The first method used here, .findOneAndDelete(), works a lot like .findOneAndUpdate(),
+  // as it deletes the document while also returning its data. We then take that data and
+  // use it to identify and remove it from the associated pizza using the Mongo $pull
+  // operation. Lastly, we return the updated pizza data, now without the _id of the comment
+  // in the comments array, and return it to the user.
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then((deletedComment) => {
+        if (!deletedComment) {
+          return res.status(400).json({ message: "No comment with this id" });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = commentController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,42 @@
 const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  // virtual function to use the to return the comment counts
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    // set id to false, virtual functions dont need ids
+    id: false,
+  }
+);
+
+// get the total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comment.length;
 });
 
 const Pizza = model("Pizza", PizzaSchema);
